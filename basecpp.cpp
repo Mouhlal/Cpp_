db851ca6850145a99ce1ac8f411f60b98b131f92
Mouhlal <akramemouhlal@gmail.com>
2024-10-11 20:28:01 +0100

    first commit

---
+++ basecpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std :: cout << "Line 1\n";
+  std :: cout << "Line 2\n";
+  std :: cout << "Line 3\n";
+  cout << 3;
+  cout << endl ;
+  cout << 26*2 ;
+
+  return 0;
+}
+
+ /* please stay with me  */
